9e5de222c1a3cb116c41b997f389663a5e359ea6
cyu <chloeyu100@gmail.com>
2021-03-06 21:45:33 -0500

    index complete

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// This JS file contains the site functions
+
+var startButton = document.querySelector("#start-button");
+var timerEl = document.querySelector('#timer-text');
+var win = document.querySelector('#win');
+var loss = document.querySelector('#loss');
+var result = document.querySelector('#result')
+
+var correct = true;
+var timer;
+
+var questions = 
+["a", 
+"b", 
+"c", 
+"d"];
